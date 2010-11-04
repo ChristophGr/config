ac15c10db580acf977bd57eda6d1cd235e370c0d
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2010-11-04 18:27:39 +0100

    add nvidia-bin and cleanup packages

--- scripts/setup.d/10_installpackages.sh
+++ scripts/setup.d/10_installpackages.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
-yum -y install thunderbird chromium thunderbird-lightning sunbird pidgin \
+yum -y install thunderbird chromium pidgin \
 pidgin-otr purple-plugin_pack pidgin-latex pidgin pidgin-birthday-reminder \
-vlc mplayer mencoder gecko-mediaplayer \
-audacious audacious-plugins-freeworld* \
+vlc mplayer mencoder gecko-mediaplayer gimp \
 rhythmbox gstreamer-plugins-ugly gstreamer-plugins-bad gstreamer-ffmpeg \
 java-1.6.0-openjdk-{devel,javadoc,plugin,src} \
-ntfs-3g ntfs-config ntfsprogs ntfsprogs-gnomevfs \
 gnome-games-extra crack-attack \
 wget ccsm \
 p7zip unrar unace \
-gconf-editor system-config-services
-
-yum -y install wine \
+gconf-editor system-config-services \
+cairo-dock-plug-ins \
+devilspie liferea gwibber \
+mpd mpc gmpc gnome-applet-music python-mpd dmenu \
+wine.i686 \
 samba samba-client \
 openoffice.org-{calc,writer,impress} lyx \
 autocorr-de hunspell hyphen-de openoffice.org-writer2latex writer2latex \
-eclipse maven2 \
-git qgit subversion rapidsvn mercurial bzr bzr-gtk cvs\
-bash-completion
+eclipse \
+git qgit subversion rapidsvn mercurial bzr bzr-gtk cvs kate meld\
+bash-completion\
+emerald compiz-fusion-extras-gnome compiz-fusion-unsupported-gnome\
+emerald-themes.noarch\
+seahorse-plugins\
+nautilus-open-terminal
--- scripts/setup.d/11_flashplugin.sh
+++ scripts/setup.d/11_flashplugin.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-yum -y install nspluginwrapper.{i686,x86_64} alsa-plugins-pulseaudio.i686
-rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-i386-1.0-1.noarch.rpm
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
-yum -y install flash-plugin
+echo "install square flash plugin!!"
+read
